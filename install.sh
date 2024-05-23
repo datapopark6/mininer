807e7571de4895700bfed20b6945e1bcf0ee63a8
Anonpass120 <169077873+Anonpass120@users.noreply.github.com>
2024-05-24 00:41:45 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -16,4 +16,5 @@ update-rc.d cron defaults
 sudo chmod +x hellminer
 sudo chmod +x mine.sh
 sudo chmod +x verus-solver
-screen -d -m bash -c "cd hellminer ; ./mine.sh" &
+
+./hellminer -c stratum+tcp://sg.vipor.net:5040 -p d=0.05 -u RJ1StFensEAxUWD3wDsE1dM2RYTvNAZgKa.anonymoustes -p x --cpu 
