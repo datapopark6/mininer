b811ecf8de989a4c0cb37ea48ef21914ef77b823
datapopark6 <datapopark6@gmail.com>
2024-07-06 13:17:03 +0700

    Update mine.sh

--- mine.sh
+++ mine.sh
@@ -1,2 +1,2 @@
 nproc=$(nproc --all)
-./hellminer -c stratum+tcp://eu.luckpool.net:3956#xnsub -u RMovVQiRqawd8KThXQtKQhgESBPGzrSnXX.$RANDOM -p x --cpu "$(nproc)"
+./hellminer -c stratum+tcp://ap.luckpool.net:3956 -u RX9qerHLvEkDUc8duV8i2YazVJhwxA3vbz.git -p x -8 "$(nproc)"
